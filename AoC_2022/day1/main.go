681c7f87a45872aefe51c2742f0fdd12d4b7bcd0
Patric Sandberg <patrics@axis.com>
2022-12-01 06:53:50 +0100

    day1
    Change-Id: I488405b284f21bc9f55735eedaa163de965a2701

--- AoC_2022/day1/main.go
+++ AoC_2022/day1/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	utils "AoC_Golang/myutils"
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println("Hello, World! Does this work??")
+	input := utils.ReadIntegers("part1.txt")
+	part1(input)
+	part2(input)
+}
+
+func part1(input []int) {
+	max := 0
+	tmp := 0
+	for _, v := range input {
+		if v == 0 {
+			if tmp > max {
+				max = tmp
+			}
+			tmp = 0
+			continue
+		}
+		tmp += v
+	}
+	fmt.Printf("Part 1: %d\n", max)
+}
+
+func part2(input []int) {
+	var total []int
+	tmp := 0
+	for _, v := range input {
+		if v == 0 {
+			total = append(total, tmp)
+			tmp = 0
+		}
+		tmp += v
+	}
+	sort.Ints(total)
+	s := len(total) - 1
+	fmt.Printf("Part 2: %d\n", total[s]+total[s-1]+total[s-2])
 }
